d0be4256e94c9a57fd8fa852116f6e43b28f257f
Pierre Josselin <contact@pierre-josselin.fr>
2023-04-28 11:42:50 +0200

    Improve keyrock config tool to automatically write env variables

--- tools/keyrock-config-tool/index.js
+++ tools/keyrock-config-tool/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const dotenv = require("dotenv");
+const fs = require("fs").promises;
 const path = require("path");
 
 const rootDirectoryPath = path.dirname(path.dirname(__dirname));
@@ -56,9 +57,7 @@ async function main() {
         }
     ];
 
-    const pepProxies = [];
-
-    for (const [index, application] of applications) {
+    for (let [index, application] of applications) {
         process.stdout.write(`Creating "${application.name}" application...`);
 
         data = {
@@ -66,14 +65,14 @@ async function main() {
         };
 
         response = await keyrockAPI.post("/applications", data);
-        applications[index] = response.data.application;
+        application = response.data.application;
 
         process.stdout.write(" OK\n");
 
         process.stdout.write("Creating pep proxy...");
 
-        response = await keyrockAPI.post(`/applications/${applications[index].id}/pep_proxies`);
-        pepProxies.push(response.data.pep_proxy);
+        response = await keyrockAPI.post(`/applications/${application.id}/pep_proxies`);
+        const pepProxy = response.data.pep_proxy;
 
         process.stdout.write(" OK\n");
 
@@ -93,7 +92,7 @@ async function main() {
                 }
             }
 
-            response = await keyrockAPI.post(`/applications/${applications[index].id}/permissions`, data);
+            response = await keyrockAPI.post(`/applications/${application.id}/permissions`, data);
             permissions.push(response.data.permission);
         }
 
@@ -107,7 +106,7 @@ async function main() {
             }
         };
 
-        response = await keyrockAPI.post(`/applications/${applications[index].id}/roles`, data);
+        response = await keyrockAPI.post(`/applications/${application.id}/roles`, data);
         const role = response.data.role;
 
         process.stdout.write(" OK\n");
@@ -115,28 +114,33 @@ async function main() {
         process.stdout.write("Assigning permissions to role...");
 
         for (const permission of permissions) {
-            await keyrockAPI.put(`/applications/${applications[index].id}/roles/${role.id}/permissions/${permission.id}`);
+            await keyrockAPI.put(`/applications/${application.id}/roles/${role.id}/permissions/${permission.id}`);
         }
 
         process.stdout.write(" OK\n");
 
         process.stdout.write("Assigning role to admin user...");
 
-        await keyrockAPI.put(`/applications/${applications[index].id}/users/${user.id}/roles/${role.id}`);
+        await keyrockAPI.put(`/applications/${application.id}/users/${user.id}/roles/${role.id}`);
+
+        process.stdout.write(" OK\n\n");
+
+        process.stdout.write("Writing variables to env file...");
+
+        if (index === 0) {
+            data = `\nWILMA_CONTEXT_BROKER_APP_ID="${application.id}"\n`;
+            data += `WILMA_CONTEXT_BROKER_APP_USERNAME="${pepProxy.id}"\n`;
+            data += `WILMA_CONTEXT_BROKER_APP_PASSWORD="${pepProxy.password}"\n`;
+        } else {
+            data = `\nWILMA_MINTAKA_APP_ID="${application.id}"\n`;
+            data += `WILMA_MINTAKA_APP_USERNAME="${pepProxy.id}"\n`;
+            data += `WILMA_MINTAKA_APP_PASSWORD="${pepProxy.password}"\n`;
+        }
+
+        await fs.appendFile([rootDirectoryPath, ".env"].join(path.sep), data, { encoding: "utf-8" })
 
         process.stdout.write(" OK\n\n");
     }
-
-    process.stdout.write("Update the .env file with the following values:\n\n");
-    process.stdout.write(`WILMA_CONTEXT_BROKER_APP_ID="${applications[0].id}"\n`);
-    process.stdout.write(`WILMA_CONTEXT_BROKER_APP_USERNAME="${pepProxies[0].id}"\n`);
-    process.stdout.write(`WILMA_CONTEXT_BROKER_APP_PASSWORD="${pepProxies[0].password}"\n\n`);
-    process.stdout.write(`WILMA_MINTAKA_APP_ID="${applications[1].id}"\n`);
-    process.stdout.write(`WILMA_MINTAKA_APP_USERNAME="${pepProxies[1].id}"\n`);
-    process.stdout.write(`WILMA_MINTAKA_APP_PASSWORD="${pepProxies[1].password}"\n\n`);
-    process.stdout.write("Then restart the docker compose script:\n\n");
-    process.stdout.write("sudo docker compose down\n");
-    process.stdout.write("sudo docker compose up -d\n");
 }
 
 main();
